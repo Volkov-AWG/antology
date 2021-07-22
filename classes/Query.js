95f465355d40e902d8a2e408d3a2df0c4d8127d2
Anton Volkov <54065904+Volkov-AWG@users.noreply.github.com>
2021-07-22 12:25:33 +0300

    Create-ui

--- classes/Query.js
+++ classes/Query.js
@@ -2,21 +2,27 @@ class Query {
     constructor(table) {
         this.table = table;
     }
-
+    //Fot Swagger
     SelectTreeById(id) {
-        return `Select content from ${this.table} where id = ${id}`;
+        return `Select * from ${this.table} where id = ${id}`;
     }
     SelectIdByName(name) {
         return `Select id from ${this.table} where name = '${name}' order by id DESC limit 1`;
     }
     SelectAllTrees() {
-        return `Select id, name, content from ${this.table}`
+        return `Select id, name, description, content from ${this.table}`
     }
     SelectAllBranches() {
         return `Select id, treeID, keys from ${this.table}`
     }
     SelectBranchByTreeID(id) {
         return `Select id, treeID, keys from ${this.table} where treeID = ${id}`
+    }
+    SelectUrlByBranchID(id) {
+        return `Select treeID, branchID, name, annotation, link, authors, journal, journal_link from ${this.table} where branchID = ${id}`
+    }
+    SelectUrl() {
+        return `Select treeID, branchID, name, annotation, link, authors, journal, journal_link from ${this.table}`
     }
     SelectUrlByTreeID(id) {
         return `Select treeID, branchID, name, annotation, link, authors, journal, journal_link from ${this.table} where treeID = ${id}`
@@ -37,5 +43,23 @@ class Query {
         console.log()
         return `delete from ${this.table} where link like '%cyberleninka.ru%' and treeid = ${data};`
     }
+
+    //For UI
+    SelectTreeForUi(name, desc){
+        console.log()
+        return `select * from ${this.table} where name ilike '%${name}%' and description ilike '%${desc}%'`
+    }
+    SelectBranchForUi(id,keys) {
+        console.log()
+        return `Select * from ${this.table} where keys ilike '%${keys}%' ${id}`
+    }
+    SelectUrlForUi(tid,bid, treename, keys, name, auth, jour){
+        console.log()
+        return `select ur.treeid, ur.branchid, tr.name as treename, br.keys, ur.name, ur.annotation, ur.link, ur.authors, ur.journal, ur.journal_link 
+                from urllist ur inner join anthologytree tr on (tr.id = ur.treeid)
+				inner join branch br on (br.id = ur.branchid)
+                where tr.name ilike '%${treename}%' and br.keys ilike '%${keys}%' and ur.name ilike '%${name}%' and ur.authors ilike '%${auth}%'and ur.journal ilike '%${jour}%' ${tid} ${bid}`
+    }
+
 }
 module.exports = Query;
